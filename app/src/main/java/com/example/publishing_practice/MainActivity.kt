defe88c38a9ec58fde7beb8cf3dd959a4e54175a
Cjsghkd <pjhomg5530@naver.com>
2023-10-05 19:06:33 +0900

    :bug: :: 애뮬레이터가 안 켜지던 오류 수정

--- app/src/main/java/com/example/publishing_practice/MainActivity.kt
+++ app/src/main/java/com/example/publishing_practice/MainActivity.kt
@@ -2,11 +2,18 @@ package com.example.publishing_practice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.publishing_practice.databinding.ActivityMainBinding
+
+private lateinit var binding: ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        binding.button.setOnClickListener {
+            ProfileBottomSheetDialogFragment().show(supportFragmentManager, "BottomSheet")
+        }
+    }
 }
