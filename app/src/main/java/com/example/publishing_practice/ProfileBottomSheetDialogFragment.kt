96eae0feba25c2fe12f8fc58874cf551ce50206a
Cjsghkd <pjhomg5530@naver.com>
2023-10-05 17:35:56 +0900

    :lipstick: :: 달라 - 프로필 바텀 시트 퍼블리싱

---
+++ app/src/main/java/com/example/publishing_practice/ProfileBottomSheetDialogFragment.kt
@@ -0,0 +1,21 @@
+package com.example.publishing_practice
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+class ProfileBottomSheetDialogFragment : BottomSheetDialogFragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_profile_bottom_sheet_dialog, container, false)
+    }
+
+    companion object {
+        const val TAG = "fragment_profile_bottom_sheet_dialog"
+    }
+}
